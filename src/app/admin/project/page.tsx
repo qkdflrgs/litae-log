d45b860148d3886e1338cde309b61a6e470c1089
qkdflrgs <qkdflrgs@gmail.com>
2024-06-25 22:52:59 +0900

    refactor(admin): refactoring project admin page

--- src/app/admin/project/page.tsx
+++ src/app/admin/project/page.tsx
@@ -1,240 +1,56 @@
 'use client'
 
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
-import rehypeHighlight from 'rehype-highlight'
-import 'highlight.js/styles/atom-one-dark-reasonable.css'
-import React, { ChangeEvent, useEffect, useState, KeyboardEvent } from 'react'
-import useNewProject from '@hooks/useNewProject'
+import useProjects from '@/hooks/useProjects'
+import transformDate from '@/utils/transformDate'
+import Link from 'next/link'
 
-export default function NewProjectPage() {
-  const [index, setIndex] = useState<string>('')
-  const [category, setCategory] = useState<string>('')
-
-  const {
-    newProjectData,
-    setNewProjectData,
-    resetNewProjectData,
-    submitNewProject,
-  } = useNewProject()
-  // const possibleSubmit =
-  //   newPostData.title.length === 0 && newPostData.category.length === 0
-
-  const handleMarkdown = (e: ChangeEvent<HTMLTextAreaElement>) => {
-    setNewPostData((prev) => ({
-      ...prev,
-      content: e.target.value,
-    }))
-  }
-
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { id, value },
-    } = e
-
-    if (id === 'title') {
-      setNewPostData((prev) => ({
-        ...prev,
-        title: value,
-      }))
-    }
-
-    if (id === 'description') {
-      setNewPostData((prev) => ({
-        ...prev,
-        description: value,
-      }))
-    }
-
-    if (id === 'category') {
-      setCategory(value)
-    }
-
-    if (id === 'index') {
-      setIndex(value)
-    }
-  }
-
-  const handleAddCategory = () => {
-    const currentCategory = newPostData.category
-    setNewPostData((prev) => ({
-      ...prev,
-      category: [...currentCategory, category],
-    }))
-    setCategory('')
-  }
-
-  const handleAddIndex = () => {
-    const currentIndex = newPostData.index
-    setNewPostData((prev) => ({ ...prev, index: [...currentIndex, index] }))
-    setIndex('')
-  }
-
-  const handleDeleteCategory = (deleteCategory: string) => {
-    const updatedCategories = newPostData.category.filter(
-      (item) => item !== deleteCategory,
-    )
-    setNewPostData((prev) => ({
-      ...prev,
-      category: updatedCategories,
-    }))
-  }
-
-  const handleDeleteIndex = (deleteIndex: string) => {
-    const updatedIndex = newPostData.index.filter(
-      (item) => item !== deleteIndex,
-    )
-    setNewPostData((prev) => ({
-      ...prev,
-      index: updatedIndex,
-    }))
-  }
-
+export default function ProjectAdminPage() {
+  const { data: projects } = useProjects()
   return (
-    <div className="flex gap-10">
-      <div className="flex flex-col gap-6 flex-1">
-        <div className="flex flex-col gap-2">
-          {/* 제목 */}
-          <label htmlFor="title">제목</label>
-          <input
-            id="title"
-            placeholder="포스트 제목을 입력해주세요"
-            value={newPostData.title}
-            onChange={handleInput}
-            className="rounded-md p-2 outline-green-800"
-            type="text"
-          />
-        </div>
-        {/* 설명 */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="description">설명</label>
-          <input
-            id="description"
-            placeholder="포스트 설명을 입력해주세요"
-            value={newPostData.description}
-            onChange={handleInput}
-            className="rounded-md p-2 outline-green-800"
-            type="text"
-          />
-        </div>
-        {/* 내용 */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="contents">내용</label>
-          <textarea
-            id="contents"
-            placeholder="포스트 내용을 입력해주세요"
-            value={newPostData.content}
-            className="w-full h-[600px] resize-none outline-green-800 p-2 rounded-md"
-            onChange={handleMarkdown}
-          />
-        </div>
-        {/* 목차 */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="index">목차</label>
-          <div className="flex flex-wrap gap-2 bg-white min-h-[50px] rounded-md p-2">
-            {newPostData.index.map((idx) => (
-              <div
-                key={idx}
-                className="flex px-2 h-[24px] bg-[#276955] rounded-sm text-white text-[14px] items-center cursor-pointer"
-                onClick={() => handleDeleteIndex(idx)}
-              >
-                <span>{idx}</span>
-              </div>
-            ))}
-          </div>
-          <div className="flex gap-4 items-center">
-            <input
-              id="index"
-              placeholder="목차를 입력해주세요"
-              value={index}
-              className="w-full rounded-md p-2 outline-green-800"
-              type="text"
-              onChange={handleInput}
-            />
-            <button
-              className="w-[60px] h-[80%] rounded-lg text-white bg-[#276955]"
-              onClick={handleAddIndex}
-            >
-              추가
-            </button>
-          </div>
-        </div>
-        {/* 카테고리 */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="category">카테고리</label>
-          <div className="flex flex-wrap gap-2 bg-white min-h-[50px] rounded-md p-2">
-            {newPostData.category.map((category) => (
-              <div
-                key={category}
-                className="flex px-2 h-[24px] bg-[#276955] rounded-sm text-white text-[14px] items-center cursor-pointer"
-                onClick={() => handleDeleteCategory(category)}
-              >
-                <span>{category}</span>
-              </div>
-            ))}
-          </div>
-          <div className="flex gap-4 items-center">
-            <input
-              id="category"
-              placeholder="카테고리를 입력해주세요"
-              value={category}
-              className="w-full rounded-md p-2 outline-green-800"
-              type="text"
-              onChange={handleInput}
-            />
-            <button
-              className="w-[60px] h-[80%] rounded-lg text-white bg-[#276955]"
-              onClick={handleAddCategory}
-            >
-              추가
-            </button>
-          </div>
-        </div>
-        {/* 이미지 */}
-        <div className="flex flex-col gap-2">
-          <label htmlFor="image">이미지</label>
-          <input type="file" />
-        </div>
-        <div className="flex gap-8 justify-center">
-          <button
-            className="w-[80px] h-[32px] bg-red-500 hover:bg-red-400 text-[16px] text-white font-bold rounded-md"
-            onClick={resetNewPostData}
-          >
-            초기화
-          </button>
-          <button
-            className="w-[80px] h-[32px] bg-green-500 hover:bg-green-400 disabled:bg-green-100 text-[16px] text-white font-bold rounded-md"
-            onClick={submitNewPost}
-            disabled={possibleSubmit}
-          >
-            등록하기
-          </button>
-        </div>
-      </div>
-      <div className="flex flex-col flex-1 h-[800px] overflow-y-scroll gap-4">
-        <span className="text-[20px] text-[#276955] font-bold">미리보기</span>
-        <ReactMarkdown
-          className="prose flex flex-col w-full"
-          components={{
-            h2(props) {
-              const { children, node, ...rest } = props
-              const id = React.Children.toArray(children)
-                .join('')
-                .replaceAll(' ', '')
-
-              return (
-                <h2 id={id} {...rest}>
-                  {children}
-                </h2>
-              )
-            },
-          }}
-          rehypePlugins={[rehypeHighlight]}
-          remarkPlugins={[remarkGfm]}
+    <div className="flex flex-col gap-8">
+      <div className="flex justify-end">
+        <Link
+          href="/admin/project/new"
+          className="px-4 py-2 bg-[#276955] hover:opacity-80 justify-center items-center text-white text-[16px] font-bold rounded-lg"
         >
-          {newPostData.content}
-        </ReactMarkdown>
+          프로젝트 작성하기
+        </Link>
       </div>
+      <table>
+        <caption className="text-start text-[24px] font-bold mb-8">
+          프로젝트 목록
+        </caption>
+        <thead className="h-[30px] bg-green-200">
+          <tr>
+            <th>아이디</th>
+            <th>제목</th>
+            <th>시작일자</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody className="">
+          {projects?.map((project) => (
+            <tr key={project.id} className="h-10">
+              <th>{project.id}</th>
+              <th>{project.title}</th>
+              <th>{transformDate(project.startedAt)}</th>
+              <th>
+                <Link
+                  className="px-4 py-1 text-white text-[14px] bg-gray-700 hover:bg-gray-600 rounded-lg"
+                  href={`/admin/project/${project.id}`}
+                >
+                  수정
+                </Link>
+              </th>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+      {projects?.length === 0 && (
+        <div className="w-full flex justify-center items-center text-gray-500">
+          <span>게시글이 없습니다.</span>
+        </div>
+      )}
     </div>
   )
 }
