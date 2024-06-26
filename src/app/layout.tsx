f4c32b30188fcb7f88b46f1b694167e88efdb335
litae <109706689+qkdflrgs@users.noreply.github.com>
2024-06-27 02:26:30 +0900

    feat(layout): set Layout page (#15)
    * feat(layout): set global style
    
    * feat(layout): add Layout page
    
    * feat(layout): add ReactQueryProvider component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Footer from '@shared/Footer'
+import Header from '@/components/shared/Header'
+import ReactQueryProviders from '@/components/shared/QueryProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,8 +18,16 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko" className="scroll-smooth">
+      <body className={` bg-[#F0F2F5]`}>
+        <ReactQueryProviders>
+          <Header />
+          <main className="max-w-[1024px] mx-auto py-[36px] lg:py-[76px] px-6 lg:px-8">
+            {children}
+          </main>
+          <Footer />
+        </ReactQueryProviders>
+      </body>
     </html>
   )
 }
