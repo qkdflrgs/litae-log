118b43980772d2380c759a21770406aed9470da9
qkdflrgs <qkdflrgs@gmail.com>
2024-07-02 23:57:32 +0900

    feat(darkmode): add custom colors for darkmode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from 'tailwindcss'
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -8,16 +9,31 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        light: {
+          bg: '#F0F2F5',
+          primary: '#276955',
+          border: '#A2A2A2',
+        },
+        dark: {
+          primary: '#8BC34A',
+          dp00: '#121212',
+          dp01: '#1e1e1e',
+          dp02: '#232323',
+          dp03: '#252525',
+          dp04: '#272727',
+          dp06: '#2c2c2c',
+          dp08: '#2e2e2e',
+          dp12: '#333333',
+          dp16: '#363636',
+          dp24: '#383838',
+        },
       },
     },
+    fontFamily: {
+      SUITRegular: ['SUIT-Regular'],
+    },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide'),
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require('tailwind-scrollbar-hide')],
 }
 export default config
