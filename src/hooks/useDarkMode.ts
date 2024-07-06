e10042b23d6e75dbf122c474b73a2255b7ea5bf3
qkdflrgs <qkdflrgs@gmail.com>
2024-07-07 04:09:53 +0900

    feat(hooks): refactoring useDarkMode hook

--- src/hooks/useDarkMode.ts
+++ src/hooks/useDarkMode.ts
@@ -1,14 +1,34 @@
+'use client'
+
 import { useEffect, useState } from 'react'
 
 export default function useDarkMode() {
-  const [darkMode, setDarkMode] = useState<boolean>(false)
+  const [darkMode, setDarkMode] = useState<boolean>(() => {
+    const theme = localStorage.getItem('litae-theme')
+    if (theme === null) {
+      return (
+        window.matchMedia &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches
+      )
+    }
+    return theme === 'dark'
+  })
+
   const toggleDarkMode = () => {
+    darkMode
+      ? localStorage.setItem('litae-theme', 'light')
+      : localStorage.setItem('litae-theme', 'dark')
     setDarkMode(!darkMode)
   }
 
   useEffect(() => {
     const html = document.querySelector('html')
-    darkMode ? html?.classList.add('dark') : html?.classList.remove('dark')
+
+    if (darkMode) {
+      html?.classList.add('dark')
+    } else {
+      html?.classList.remove('dark')
+    }
   }, [darkMode])
 
   return { darkMode, toggleDarkMode }
