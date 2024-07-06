73d14a3d99743e8e58dc842bcca94808556e2264
qkdflrgs <qkdflrgs@gmail.com>
2024-07-07 04:15:51 +0900

    refactor(main): refactoring layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
 import Footer from '@shared/Footer'
 import Header from '@shared/Header'
 import ReactQueryProviders from '@shared/QueryProvider'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'LITAE LOG',
@@ -19,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko" className="scroll-smooth">
-      <body className={` bg-[#F0F2F5]`}>
+      <body className={'bg-light-bg dark:bg-dark-dp01 relative'}>
         <ReactQueryProviders>
           <Header />
           <main className="max-w-[1024px] mx-auto py-[36px] lg:py-[76px] px-6 lg:px-8">
