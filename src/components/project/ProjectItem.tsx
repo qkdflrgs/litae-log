e5f3933db6169dd721e552e12e18662cc2703564
qkdflrgs <qkdflrgs@gmail.com>
2024-07-07 03:52:09 +0900

    feat(shared): add ProjectItem component

--- src/components/project/ProjectItem.tsx
+++ src/components/project/ProjectItem.tsx
@@ -1,9 +1,11 @@
 import Link from 'next/link'
+import Tag from '../shared/Tag'
 
 interface ProjectItemProps {
   id: string
   tag: string
   title: string
+  description: string
   thumbnailImage: string
 }
 
@@ -11,28 +13,27 @@ export default function ProjectItem({
   id,
   tag,
   title,
+  description,
   thumbnailImage,
 }: ProjectItemProps) {
   return (
     <Link
       href={`/project/${id}`}
-      className="w-full h-[360px] relative before:content-[''] before:absolute before:top-0 before:left-0 before:w-[100%] before:h-[360px] before:bg-gray-500 before:opacity-40 hover:before:opacity-60 before:rounded-md"
+      className="w-full max-h-[300px] relative before:content-[''] before:absolute before:top-0 before:left-0 before:w-[100%] before:h-[100%] before:bg-gray-500 before:opacity-40 hover:before:opacity-60 before:rounded-md lg:h-[360px]"
     >
       <img
         className="w-full h-full object-cover rounded-md"
-        src={thumbnailImage}
+        src={!thumbnailImage ? 'default.webp' : thumbnailImage}
         alt={title}
       />
       <div className="absolute bottom-[20px] px-5 flex flex-col gap-2">
         <div className="flex gap-4">
-          <div className="text-[12px] text-black bg-white font-semibold px-2 py-1 rounded-3xl">
-            {tag}
-          </div>
+          <Tag type="project" label={tag} />
         </div>
-        <span className="text-[28px] text-white font-bold">{title}</span>
-        <p className="text-[12px] text-gray-400 font-bold">
-          배달의 민족 프로젝트는 배달 앱의 쿠폰시스템을 클론한 프로젝트입니다.
-        </p>
+        <span className="text-[16px] lg:text-[28px] text-white color-gray-600 font-bold">
+          {title}
+        </span>
+        <p className="text-[12px] text-gray-200 font-bold">{description}</p>
       </div>
     </Link>
   )
